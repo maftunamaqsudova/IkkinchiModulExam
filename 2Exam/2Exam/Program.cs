ed7ac744bbaa2ce0b98599fa2785a54453face22
maftunamaqsudova <maftuna.starr@gmail.com>
2026-01-29 13:23:36 +0500

    exam2

---
+++ 2Exam/2Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _2Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
